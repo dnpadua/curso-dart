dfeb670aae8913a288c957a52ebd1a9d3bf614b8
dnpadua <37667481+dnpadua@users.noreply.github.com>
2021-08-19 00:13:39 -0300

    Loops com Dart

--- Projetos/Loops/loops.dart
+++ Projetos/Loops/loops.dart
@@ -1,7 +1,13 @@
 main() {
-  var numero = 10;
+  var numero = 3;
   while (numero >= 0) {
     print("Contagem regressiva: $numero");
     numero--;
   }
+
+  var contagem = 1;
+  do {
+    print("Contando: $contagem");
+    contagem++;
+  } while (contagem <= 3);
 }
